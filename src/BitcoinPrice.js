8e9eb20f5d369f77c5805289d558497b96f9b0ed
jaghadai <42021038+jaghadai@users.noreply.github.com>
2019-01-29 19:17:26 -0800

    trying again

--- src/BitcoinPrice.js
+++ src/BitcoinPrice.js
@@ -1,31 +1,31 @@
 import React, { Component } from "react";
 
 class BitcoinPrice extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			bitcoinprice: {}
-		};
-	}
-	componentDidMount() {
-		fetch("https://api.coindesk.com/v1/bpi/currentprice.json").then(
-			Response =>
-				this.setState(
-					this.state.bitcoinprice,
-					JSON.parse(Response.json)
-				)
-		);
-	}
+  constructor(props) {
+    super(props);
+    this.state = {
+      bitcoinprice: {}
+    };
+  }
+  componentDidMount() {
+    fetch("https://api.coindesk.com/v1/bpi/currentprice.json")
+      .then(Response => {
+        return Response.json();
+      })
+      .then(Result => {
+        this.setState({ bitcoinprice: JSON.parse(Result) });
+      })
+      .then(console.log(this.state.bitcoinprice.bpi.USD.rate));
+  }
 
-	render() {
-		if (Object.keys(this.state.bitcoinprice).length == 0) {
-			return <div>Bitcoin Prices are Loading</div>;
-		} else {
-			return (
-				<div className="ticker">{this.state.bitcoinprice.bpi.rate}</div>
-			);
-		}
-	}
+  render() {
+    if (Object.keys(this.state.bitcoinprice).length === 0) {
+      return <div className="ticker"> Bitcoins!</div>;
+    } else {
+      return (
+        <div className="ticker">{this.state.bitcoinprice.bpi.USD.rate}</div>
+      );
+    }
+  }
 }
-
 export default BitcoinPrice;
